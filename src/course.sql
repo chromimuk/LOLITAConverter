87099db93b7c608df92974d5e9b86815b51b2588
Benjamin Marconnet <benjamin.marconnet@gmail.com>
2014-06-13 16:31:45 +0200

    new example data

--- src/course.sql
+++ src/course.sql
@@ -1,26 +1,5 @@
--- -----------------------------------------------------------------------------
---             Génération d'une base de données pour
---                      Oracle Version 10g
---                        (8/4/2014 16:30:47)
--- -----------------------------------------------------------------------------
---      Nom de la base : MLR3
---      Projet : Course à pied
---      Auteur : etudiant etudiant
---      Date de dernière modification : 8/4/2014 16:30:04
--- -----------------------------------------------------------------------------
-
-DROP TABLE COURSE CASCADE CONSTRAINTS;
-
--- -----------------------------------------------------------------------------
---       TABLE : COURSE
--- -----------------------------------------------------------------------------
-
-
-CREATE TABLE COURSE
+CREATE TABLE LANGUE
 (
-    NUMCOURSE NUMBER(3)  NOT NULL,
-    LIBCOURSE VARCHAR(20)  NOT NULL,
-    VILLECOURSE VARCHAR(45)  NOT NULL,
-    DATECOURSE DATE  NOT NULL,
-	CONSTRAINT PK_COURSE PRIMARY KEY (NUMCOURSE)  
+    NUMLANGUE NUMBER(2)  NOT NULL,
+    LIBLANGUE VARCHAR2(30)  NOT NULL
 );
