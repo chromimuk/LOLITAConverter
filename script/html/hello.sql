9b902b01b1a1415cbf0b1f736e9a059eea7b8df8
Benjamin Marconnet <benjamin.marconnet@gmail.com>
2014-06-18 17:09:12 +0200

    Update hello.sql

--- script/html/hello.sql
+++ script/html/hello.sql
@@ -14,7 +14,11 @@ BEGIN
 		htp.headClose;
 		htp.bodyOpen;
 			htp.print('<div class="container">');
-			htp.header(1,'LOLITA');
+			htp.print('<div class="jumbotron">');
+			htp.print('<a href="hello">');
+			htp.print('<img src="https://dl.dropboxusercontent.com/u/21548623/LOGOLOLITA.PNG" width="300px" style="display:block; margin-left:auto; margin-right: auto;" />');
+			htp.print('</a>');
+			htp.print('</div>');
 			if (cookie_lolita.num_vals > 0)
 			then 
 			    user_id  := cookie_lolita.vals(1);
@@ -27,19 +31,15 @@ BEGIN
 			htp.print('<a class="btn btn-danger" href="admin_expert" >Administration expert</a>');
 			htp.br;
 			htp.br;
-			htp.print('<div class="jumbotron">');
-			htp.print('<a href="hello">');
-			htp.print('<img src="https://dl.dropboxusercontent.com/u/21548623/LOGOLOLITA.PNG" width="300px" style="display:block; margin-left:auto; margin-right: auto;" />');
-			htp.print('</a>');
-			htp.print('</div>');
-			htp.br;
 			htp.header(2,'Nos domaines de compétences');
 			htp.print('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.');
+			htp.br;
 			htp.br;
 			htp.print('<a class="btn btn-primary" href="afft_domaine" >Consulter nos domaines de compétences</a>');
 			htp.hr;
 			htp.header(2,'Nos experts');
 			htp.print('Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.');
+			htp.br;
 			htp.br;
 			htp.print('<a class="btn btn-primary" href="afft_membre_from_typmembre?vtypmembre=E" >En savoir plus sur nos experts</a>');
 			htp.hr;
