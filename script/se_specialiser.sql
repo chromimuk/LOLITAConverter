ed41ce402a727993c8923005fdee0a3f365f7590
Benjamin Marconnet <benjamin.marconnet@gmail.com>
2014-06-19 13:58:11 +0200

    Update se_specialiser.sql

--- script/se_specialiser.sql
+++ script/se_specialiser.sql
@@ -45,8 +45,6 @@ BEGIN
 	htp.tableData(rec.nummembre);
 	htp.tableData(rec.premembre || ' ' || rec.nommembre);
 	htp.tableData(
-		htf.anchor('ui_frmedit_se_specialiser?vnumdomaine=' || rec.numdomaine, 'Modifier')
-		|| ' ou ' ||
 		htf.anchor('ui_execdel_se_specialiser?vnumdomaine=' || rec.numdomaine, 'Supprimer')
 	);
 	htp.tableRowClose;
@@ -150,98 +148,6 @@ END;
 /
 
 
---3 Edition
-
---3.1.1 Requête SQL
-CREATE OR REPLACE
-PROCEDURE pa_edit_se_specialiser
-	(
-		vnumdomaine in number,
-		vnummembre in number
-	)
-IS
-BEGIN
-	UPDATE 
-		SE_SPECIALISER
-	SET
-		nummembre = vnummembre
-	WHERE 
-		numdomaine = vnumdomaine;
-	COMMIT;
-END;
-/
-
-
---3.1.2 Page de validation d'édition
--------Appel à la requête pa_edit_se_specialiser
-CREATE OR REPLACE
-PROCEDURE ui_execedit_se_specialiser
-	(
-		vnumdomaine in number,
-		vnummembre in number
-	)
-IS
-rep_css varchar2(255) := 'https://dl.dropboxusercontent.com/u/21548623/bootstrap.min.css';
-BEGIN
-	htp.print('<!DOCTYPE html>');
-	htp.htmlOpen;
-	htp.headOpen;
-	htp.title('Edition table SE_SPECIALISER');
-	htp.print('<link href="' || rep_css || '" rel="stylesheet" type="text/css" />');
-	htp.headClose;
-	htp.bodyOpen;
-	htp.print('<div class="container">');
-	pa_edit_se_specialiser(vnumdomaine,vnummembre);
-	htp.header(1, '<a href="hello">');
-	htp.header(1, '<img src="https://dl.dropboxusercontent.com/u/21548623/LOGOLOLITA.PNG" width="300px" style="display:block; margin-left:auto; margin-right: auto;" />');
-	htp.header(1, '</a>');
-	htp.hr;
-	htp.header(2, 'Edition effectuée dans la table SE_SPECIALISER');
-	htp.print('<a class="btn btn-primary" href="afft_se_specialiser" >>Consulter la liste SE_SPECIALISER</a>');
-	htp.print('<a class="btn btn-primary" href="hello" >>Retour accueil</a>');
-	htp.print('</div>');
-	htp.bodyClose;
-	htp.htmlClose;
-EXCEPTION
-	WHEN OTHERS THEN
-		htp.print('ERROR: ' || SQLCODE);
-END;
-/
-
-
---3.1.3 Formulaire d'édition
-------- Validation redirige vers ui_execedit_se_specialiser
-CREATE OR REPLACE PROCEDURE ui_frmedit_se_specialiser
-IS
-	rep_css varchar2(255) := 'https://dl.dropboxusercontent.com/u/21548623/bootstrap.min.css';
-BEGIN
-	htp.print('<!DOCTYPE html>');
-	htp.htmlOpen;
-	htp.headOpen;
-	htp.title('Edition se_specialiser');
-	htp.print('<link href="' || rep_css || '" rel="stylesheet" type="text/css" />');
-	htp.headClose;
-	htp.bodyOpen;
-	htp.print('<div class="container">');
-	htp.header(1, 'Edition se_specialiser');
-	htp.formOpen(owa_util.get_owa_service_path || 'ui_execedit_se_specialiser', 'POST');
-	htp.print('<table class="table">');
-	htp.tableRowOpen;
-	htp.tableData('Numéro du domaine');
-	htp.tableData(htf.formText('vnumdomaine', 2));
-	htp.tableRowClose;
-	htp.tableRowOpen;
-	htp.tableData('Numéro du membre');
-	htp.tableData(htf.formText('vnummembre', 5));
-	htp.tableRowClose;
-	htp.tableClose;
-	htp.print('<button class="btn btn-primary" type="submit">Validation</button>');
-	htp.formClose;
-	htp.print('</div>');
-	htp.bodyClose;
-	htp.htmlClose;
-END;
-/
 
 
 --4 Suppression
