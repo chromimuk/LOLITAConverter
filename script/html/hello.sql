0b37aaf06783cf0e770cdfbbddd5d14f9f28c817
Alcidauk <piR.R25@gmail.com>
2014-06-19 13:39:59 +0200

    Update hello.sql

--- script/html/hello.sql
+++ script/html/hello.sql
@@ -2,15 +2,14 @@ CREATE OR REPLACE
 PROCEDURE hello
 IS
 	rep_css varchar2(255) := 'https://dl.dropboxusercontent.com/u/21548623/bootstrap.min.css';
-	cookie_lolita owa_cookie.cookie;
 	user_id number(5);
 	user_name varchar2(80);
 	user_type varchar2(2);
 	user_right varchar2(4);
 BEGIN
-    	get_info_user(user_id, user_name, user_type);
+	get_info_user(user_id, user_name, user_type);
     	get_info_user_right(user_right);
-
+    	
 	htp.print('<!DOCTYPE html>');
 	htp.htmlOpen;
 		htp.headOpen;
@@ -18,6 +17,82 @@ BEGIN
 			htp.print('<link href="https://dl.dropboxusercontent.com/u/21548623/bootstrap.min.css" rel="stylesheet">');
 		htp.headClose;
 		htp.bodyOpen;
+			htp.print('<div class="container">');
+
+			header(user_id, user_name, user_type, user_right);
+
+			if (user_id >= 0)
+			then 
+				htp.hr;
+				htp.header(2,'Nos domaines de compétences');
+				htp.print('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.');
+				htp.br;
+				htp.br;
+				htp.print('<a class="btn btn-primary" href="afft_domaine_user" >Consulter nos domaines de compétences</a>');
+				htp.hr;
+				htp.header(2,'Nos experts');
+				htp.print('Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.');
+				htp.br;
+				htp.br;
+				htp.print('<a class="btn btn-primary" href="afft_membre_user" >En savoir plus sur nos experts</a>');
+				htp.hr;
+				htp.header(2,'Questions/réponses');
+				htp.print('<a class="btn btn-primary" href="afft_sujet_from_type_user?vlibtypesujet=FQ" >Consulter la FAQ</a>');
+				htp.print('<a class="btn btn-primary" href="afft_sujet_from_type_user?vlibtypesujet=QR" >Consulter la liste des sujets</a>');
+				htp.print('<a class="btn btn-primary" href="ui_frmadd_sujet" >Creer un nouveau sujet</a>');
+				htp.print('</div>');
+					
+			else
+				htp.br;
+
+				htp.print('<div style="text-align: center">');
+				htp.br;
+				
+				htp.header(1, 'Des questions en informatique ?');
+				htp.br;
+				htp.header(1, 'Des problématiques difficiles à résoudre ?');
+				htp.br;
+				htp.br;
+				htp.br;
+				
+				htp.header(2,'LOLITA peut vous aider à trouver des solutions !');
+				htp.br;
+				htp.br;
+				htp.br;
+				
+				htp.header(2, 'Parlez-en à votre PDG.');
+				
+				htp.br;
+				htp.br;
+				
+				htp.print('<a class="btn btn-success" style="font-size: 50px; margin-left: auto; margin-right: 10px;" href="ui_frmadd_membre" >Inscription</a>');
+				htp.print('<a class="btn btn-primary" style="font-size: 50px; margin-right:auto;" href="login" >Connexion</a>');
+				
+				
+				htp.print('</div>');
+			end if;
+			
+		htp.bodyClose;
+	htp.htmlClose;
+	EXCEPTION
+		WHEN OTHERS THEN
+		htp.print(SQLCODE);
+END;
+/
+
+CREATE OR REPLACE
+PROCEDURE header
+	(
+	user_id in number,
+	user_name in varchar2,
+	user_type in varchar2,
+	user_right in varchar2	
+	)
+IS
+	rep_css varchar2(255) := 'https://dl.dropboxusercontent.com/u/21548623/bootstrap.min.css';
+BEGIN
+
+	htp.bodyOpen;
 			htp.print('<div class="container">');
 			
 			htp.print('<div style="display:block; background-color: #D3D3D3; height: 150px; border-radius: 3px; padding:10px;">');
@@ -54,59 +129,15 @@ BEGIN
 				htp.br;	
 				htp.br;
 				htp.br;
-				htp.hr;
-				htp.header(2,'Nos domaines de compétences');
-				htp.print('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.');
-				htp.br;
-				htp.br;
-				htp.print('<a class="btn btn-primary" href="afft_domaine_user" >Consulter nos domaines de compétences</a>');
-				htp.hr;
-				htp.header(2,'Nos experts');
-				htp.print('Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.');
-				htp.br;
-				htp.br;
-				htp.print('<a class="btn btn-primary" href="afft_membre_user" >En savoir plus sur nos experts</a>');
-				htp.hr;
-				htp.header(2,'Questions/réponses');
-				htp.print('<a class="btn btn-primary" href="afft_sujet_from_type_user?vlibtypesujet=FQ" >Consulter la FAQ</a>');
-				htp.print('<a class="btn btn-primary" href="afft_sujet_from_type_user?vlibtypesujet=QR" >Consulter la liste des sujets</a>');
-				htp.print('<a class="btn btn-primary" href="ui_frmadd_sujet" >Creer un nouveau sujet</a>');
-				htp.print('</div>');
-					
+
 			else
 				htp.br;
 				htp.print('<a class="btn btn-success" style="float: right" href="ui_frmadd_membre" >Inscription</a>');
 				htp.print('<a class="btn btn-primary" style="float: right; margin-right: 10px;" href="login" >Connexion</a>');
 				
 				htp.print('</div>');
-				
-				htp.print('<div style="text-align: center">');
-				htp.br;
-				
-				htp.header(1, 'Des questions en informatique ?');
-				htp.br;
-				htp.header(1, 'Des problématiques difficiles à résoudre ?');
-				htp.br;
-				htp.br;
-				htp.br;
-				
-				htp.header(2,'LOLITA peut vous aider à trouver des solutions !');
-				htp.br;
-				htp.br;
-				htp.br;
-				
-				htp.header(2, 'Parlez-en à votre PDG.');
-				
-				htp.br;
-				htp.br;
-				
-				htp.print('<a class="btn btn-success" style="font-size: 50px; margin-left: auto; margin-right: 10px;" href="ui_frmadd_membre" >Inscription</a>');
-				htp.print('<a class="btn btn-primary" style="font-size: 50px; margin-right:auto;" href="login" >Connexion</a>');
-				
-				
-				htp.print('</div>');
 			end if;
-			
+				
 		htp.bodyClose;
 	htp.htmlClose;
 	EXCEPTION
@@ -146,6 +177,10 @@ BEGIN
                    user_right := 'null';
 END;
 /
+
+
+
+
 
 CREATE OR REPLACE
 PROCEDURE get_info_user
